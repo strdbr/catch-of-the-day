c71333cfcaf473aac8860e7d0364f32172e5303b
Emmanuel Estrada <e.estrada1917@gmail.com>
2021-10-20 15:31:32 -0500

    Handling events

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -4,11 +4,13 @@ import { getFunName } from '../helpers';
 class StorePicker extends React.Component{
     myInput = React.createRef();
 
-    goToStore = (event) =>{
+    goToStore = event =>{
         // Detener el submit y no refrescar la pagina
-        event.preventDefautl();
+        event.preventDefault();
         // Agarrar el texto del form
-        
+        const storeName = this.myInput.current.value;
+        // Cambiar el URL a lo que esta en el form
+        this.props.history.push(`/store/${storeName}`);
     }
     render(){
         return (
@@ -17,6 +19,7 @@ class StorePicker extends React.Component{
                     <h2>Please enter a store</h2>
                     <input 
                         type='text' 
+                        ref={this.myInput}
                         required 
                         placeholder='Store Name'  
                         defaultValue={getFunName()} 
