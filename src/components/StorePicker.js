d61490122fe2021a5608a78257e724da129d54b3
Emmanuel Estrada <e.estrada1917@gmail.com>
2021-10-20 15:05:42 -0500

    Events, refs and binding

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,12 +2,25 @@ import React from "react";
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component{
+    myInput = React.createRef();
+
+    goToStore = (event) =>{
+        // Detener el submit y no refrescar la pagina
+        event.preventDefautl();
+        // Agarrar el texto del form
+        
+    }
     render(){
         return (
             <> {/* Te permite fragmentar sin emparentar el HTM*/}
-                <form className='store-selector'>
+                <form className='store-selector' onSubmit={this.goToStore}>
                     <h2>Please enter a store</h2>
-                    <input type='text' required placeholder='Store Name'  defaultValue={getFunName()} />
+                    <input 
+                        type='text' 
+                        required 
+                        placeholder='Store Name'  
+                        defaultValue={getFunName()} 
+                    />
                     <button type='submit'>Visit store</button>
                 </form>
             </>
