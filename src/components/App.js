c9f8a6a458256ce5ddef3eba3a5e9c63c07d79a2
Emmanuel Estrada <e.estrada1917@gmail.com>
2021-10-22 12:53:39 -0500

    Set State

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,21 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 
 class App extends React.Component{
+    state = {
+        fishes: {},
+        order: {}
+    };
+    addFish = fish =>{
+        //Editar el state
+        //1. Conseguir una copia del state existente
+        const fishes = {...this.state.fishes};
+        //2. Añadir el pescado
+        fishes[`fish${Date.now()}`] = fish;
+        //3. Añadir el pescado al state
+        this.setState({
+           fishes: fishes 
+        });
+    };
     render(){
         return(
             <div className='catch-of-the-day'>
@@ -11,7 +26,7 @@ class App extends React.Component{
                     <Header tagline='Fresh Seafood Market' />
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={this.addFish} />
             </div>
         );
     }
