71e6739a54379994b8c0423d21050811cbc23540
Emmanuel Estrada <e.estrada1917@gmail.com>
2021-11-03 14:56:42 -0600

    Remove items from state

--- src/components/App.js
+++ src/components/App.js
@@ -49,6 +49,12 @@ class App extends React.Component{
         this.setState({fishes});
     };
 
+    deleteFish = (key) => {
+        const fishes = {...this.state.fishes};
+        fishes[key] = null;
+        this.setState({ fishes });
+    }
+
     loadSampleFishes = () => {
         this.setState({fishes: sampleFishes});
     };
@@ -59,6 +65,12 @@ class App extends React.Component{
         //2.Añadir o actualizar la orden
         order[key] = order[key] + 1 || 1;
         //3. Llamar el setState para actualizar el state
+        this.setState({ order });
+    }
+
+    deleteOrder = (key) =>{
+        const order = {...this.state.order};
+        delete order[key];
         this.setState({ order });
     }
 
@@ -78,10 +90,11 @@ class App extends React.Component{
                             ))}
                     </ul>
                 </div>
-                <Order fishes={this.state.fishes} order={this.state.order} />
+                <Order fishes={this.state.fishes} order={this.state.order} deleteOrder={this.deleteOrder} />
                 <Inventory 
                     addFish={this.addFish}
-                    updateFish={this.updateFish}  
+                    updateFish={this.updateFish}
+                    deleteFish={this.deleteFish}  
                     loadSampleFishes={this.loadSampleFishes}
                     fishes={this.state.fishes}
                 />
--- src/components/Order.js
+++ src/components/Order.js
@@ -13,6 +13,7 @@ class Order extends React.Component{
         return (<li key={key}>
             {count} lbs {fish.name}
             {formatPrice(count * fish.price)}
+            <button onClick={() => this.props.deleteOrder(key)}>&times;</button>
         </li>
         )};
     render(){
